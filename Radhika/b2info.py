8e3220d92800375ca432fd8fb2f8a0b5cfae7159
Bharath2001T <121470185+Bharath2001T@users.noreply.github.com>
2024-11-14 10:00:45 +0530

    Add files via upload

---
+++ Radhika/b2info.py
@@ -0,0 +1,6 @@
+text = "AI can be used for a variety of applications, including Self-driving cars, Image recognition programs, 
+Virtual assistants like Siri and Alexa, 
+Customer service chatbots, and Automating repetitive tasks like data entry and factory work"
+
+print("text")
+
